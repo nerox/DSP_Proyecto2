2bdc3cab5ef63f188a4b9b1cec7dc887253e281b
Alejandro Vargas <nerox@users.noreply.github.com>
2018-11-26 01:20:17 -0600

    Add files via upload
    Added the functionalities for trnasmitter compresion, the receptor is also completed. The pending work to do is to separate the code into 2 independent modules

--- Transmisor_v1.m
+++ Transmisor_v1.m
@@ -2,13 +2,28 @@ pkg load signal;
 pkg load communications
 close all;
 
+function [output_signal,max_val_band,offset]=Cuantify(input_signal,bits)
+  disp(mean(input_signal));
+  offset= min(input_signal);
+  signal=abs(offset).+input_signal;
+  max_val_band=max(signal);
+  output_signal=round(signal./max_val_band*(bits^2-1));
+  %figure();
+  %plot(output_signal);  
+  output_signal=round(output_signal);
+end
+
+function output_signal=De_Cuantify(input_signal,bits,offset,max_val_band)
+  output_signal=input_signal./(bits^2-1)*max_val_band; 
+  output_signal=output_signal.+offset;  
+end
 
 
 tmax = 30;
 
 
 [smues, fmuestreo] = audioread('20181121_1339.wav');
-plot(smues);
+%plot(smues);
 
 %% Graficar la señal muestreada
 %figure();
@@ -106,7 +121,7 @@ smues4f = filter(filtro4, 1, smues);
 %title("Espectro señal banda1 filtrada 4")
 
 % ********** DECIMADO **********------------------------------------------------
-factDecimado = 3;
+factDecimado = 4;
 
 smues1 = downsample(smues1f,factDecimado);
 smues2 = downsample(smues2f,factDecimado);
@@ -172,6 +187,61 @@ smues3c = compand(smues3,Mu,v3);
 v4 = max(smues4);
 smues4c = compand(smues4,Mu,v4);
 
+% CUANTIFY  -------------------------------------------------------------------
+band1_bits=4;
+band2_bits=12;
+band3_bits=12;
+band4_bits=4;
+total_bits=band1_bits+band2_bits+band3_bits+band4_bits;
+[output_signal_band1,max_val_band1,offset_band1]=Cuantify(smues1c,band1_bits);
+[output_signal_band2,max_val_band2,offset_band2]=Cuantify(smues2c,band2_bits);
+[output_signal_band3,max_val_band3,offset_band3]=Cuantify(smues3c,band3_bits);
+[output_signal_band4,max_val_band4,offset_band4]=Cuantify(smues4c,band4_bits);
+
+band1_bin=dec2bin(output_signal_band1,band1_bits);
+band2_bin=dec2bin(output_signal_band2,band2_bits);
+band3_bin=dec2bin(output_signal_band3,band3_bits);
+band4_bin=dec2bin(output_signal_band4,band4_bits);
+
+
+%Sava data in compressed file --------------------------------------------------------
+filename = 'compressed_file.bin';
+
+output_container=strcat(band1_bin,band2_bin,band3_bin,band4_bin);
+output_container_dec=uint32(base2dec(output_container,2));
+dimension=length(output_container_dec);
+fileID = fopen(filename,'w');
+fwrite(fileID,output_container_dec,'uint32');
+fclose(fileID);
+
+% End of trnasmitter module -------------------------------------------------------------------
+
+
+% Start of receptor module -------------------------------------------------------------------
+
+%Read data in compressed file -------------------------------------------------------------------
+fileID = fopen('compressed_file.bin');
+
+input_container_dec = fread(fileID,[dimension 1],'uint32'); 
+fclose(fileID);
+input_container=dec2bin(input_container_dec);
+band1_bin= substr(input_container,1,band1_bits);
+band2_bin= substr(input_container,band1_bits+1,band2_bits);
+band3_bin= substr(input_container,band1_bits+band2_bits+1,band3_bits);
+band4_bin= substr(input_container,band1_bits+band2_bits+band3_bits+1,band4_bits);
+
+input_signal_band1=bin2dec(band1_bin);
+input_signal_band2=bin2dec(band2_bin);
+input_signal_band3=bin2dec(band3_bin);
+input_signal_band4=bin2dec(band4_bin);
+
+
+smues1c=De_Cuantify(input_signal_band1,band1_bits,offset_band1,max_val_band1);
+smues2c=De_Cuantify(input_signal_band2,band2_bits,offset_band2,max_val_band2);
+smues3c=De_Cuantify(input_signal_band3,band3_bits,offset_band3,max_val_band3);
+smues4c=De_Cuantify(input_signal_band4,band4_bits,offset_band4,max_val_band4);
+
+% Expanding -------------------------------------------------------------------
 
 %Seccion de receptor, esto luego se separa en un codigo aparte
 
@@ -180,12 +250,15 @@ smues3=compand(smues3c,Mu,v3,'mu/expander');
 smues2=compand(smues2c,Mu,v2,'mu/expander');
 smues1=compand(smues1c,Mu,v1,'mu/expander');
 
+% Interpolation -------------------------------------------------------------------
 
-smues1f=interp(smues1,3);
-smues2f=interp(smues2,3);
-smues3f=interp(smues3,3);
-smues4f=interp(smues4,3);
 
+smues1f=interp(smues1,factDecimado);
+smues2f=interp(smues2,factDecimado);
+smues3f=interp(smues3,factDecimado);
+smues4f=interp(smues4,factDecimado);
+
+% Filtering -------------------------------------------------------------------
 
 smues1f=filter(filtro1,1,smues1f);
 smues2f=filter(filtro2,1,smues2f);
@@ -193,5 +266,7 @@ smues3f=filter(filtro3,1,smues3f);
 smues4f=filter(filtro4,1,smues4f);
 
 smues_final=smues1f+smues2f+smues3f+smues4f;
+%figure();
+%plot(smues_final);
 audiowrite('20181121_1339_reconstruido.wav',smues_final,fmuestreo);
 
